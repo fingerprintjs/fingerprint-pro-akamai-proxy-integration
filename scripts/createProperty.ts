0eb61103a45166f7548c8afde66d644482cc463e
Orkun <orkuncakilkaya@gmail.com>
2023-10-31 13:08:37 +0300

    chore: add edgehostname create step

--- scripts/createProperty.ts
+++ scripts/createProperty.ts
@@ -11,10 +11,30 @@ eg.auth({
 })
 
 eg.send((err, response, body) => {
-    if(!response) {
+    if (!response) {
         process.exit(1)
     }
-    if(response?.status >= 200 && response?.status < 300) {
+    if (response?.status >= 200 && response?.status < 300) {
+        eg.auth({
+            path: `/papi/v1/edgehostnames?contractId=${process.env.AK_CONTRACT_ID}&groupId=${process.env.AK_GROUP_ID}`,
+            method: 'POST',
+            body: JSON.stringify({
+                ipVersionBehavior: 'IPV4',
+                domainPrefix: `${process.env.BRANCH_NAME}.cfi-fingerprint.com`,
+                domainSuffix: `edgesuite.net`,
+                productId: `Site_Accel`,
+            })
+        });
+
+        eg.send((err, response, body) => {
+            if (!response) {
+                process.exit(1)
+            }
+            if (response?.status >= 200 && response?.status < 300) {
+                process.exit(0)
+            }
+            process.exit(1);
+        });
         process.exit(0)
     }
     process.exit(1)
