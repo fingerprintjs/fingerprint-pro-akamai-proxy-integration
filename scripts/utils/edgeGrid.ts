4e50d8c71190d30d42d16a671c0aa30fde08b646
Orkun <orkuncakilkaya@gmail.com>
2023-10-31 12:29:19 +0300

    chore: change env structure on ci workflow

--- scripts/utils/edgeGrid.ts
+++ scripts/utils/edgeGrid.ts
@@ -1,6 +1,8 @@
 import EdgeGrid from "akamai-edgegrid";
 
-export const eg = new EdgeGrid({
-    path: '~/.edgerc',
-    section: 'papi',
-});
+const clientToken = process.env.AK_CLIENT_TOKEN ?? '',
+    clientSecret = process.env.AK_CLIENT_SECRET ?? '',
+    accessToken = process.env.AK_ACCESS_TOKEN ?? '',
+    baseUri = `https://${process.env.AK_HOST ?? ''}/`;
+
+export const eg = new EdgeGrid(clientToken, clientSecret, accessToken, baseUri);
