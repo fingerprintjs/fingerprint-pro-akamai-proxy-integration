cdde0cc8eab5ce46319244a141cfed24299cfd76
Orkun <orkuncakilkaya@gmail.com>
2023-11-07 12:53:36 +0300

    chore: add FPJS_CI_DOMAIN secret

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -1,2 +1,3 @@
-export const CI_DOMAIN = `${process.env.BRANCH_NAME}.cfi-fingerprint.com`;
-export const ORIGIN_DOMAIN = "origin-akamai.cfi-fingerprint.com";
+export const FPJS_CI_DOMAIN = `${process.env.FPJS_CI_DOMAIN}`;
+export const CI_DOMAIN = `${process.env.BRANCH_NAME}.${FPJS_CI_DOMAIN}`;
+export const ORIGIN_DOMAIN = `origin-akamai.${FPJS_CI_DOMAIN}`;
