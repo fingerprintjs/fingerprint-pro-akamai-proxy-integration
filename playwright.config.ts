60a173aa142be962d01df468c257a2ebf1c5fd68
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-12-06 12:16:55 +0200

    test: use defineConfig

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,14 @@
 import dotenv from 'dotenv'
-import { PlaywrightTestConfig } from '@playwright/test'
+import {defineConfig} from '@playwright/test'
 import {env} from "./test/e2e/utils/env";
 
 dotenv.config()
 
-const config: PlaywrightTestConfig = {
-  testDir: './test/e2e',
-  retries: 5,
-  use: {
-    ignoreHTTPSErrors: env.ignoreHTTPSErrors
+export default defineConfig({
+    testDir: './test/e2e',
+    retries: 5,
+    use: {
+      ignoreHTTPSErrors: env.ignoreHTTPSErrors
+    }
   }
-}
-
-export default config
+)
