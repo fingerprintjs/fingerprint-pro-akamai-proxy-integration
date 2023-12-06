af40719dacec2343e144572f8668f7b0376aed32
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-12-06 12:18:44 +0200

    build: add prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,13 @@
 import dotenv from 'dotenv'
-import {defineConfig} from '@playwright/test'
-import {env} from "./test/e2e/utils/env";
+import { defineConfig } from '@playwright/test'
+import { env } from './test/e2e/utils/env'
 
 dotenv.config()
 
 export default defineConfig({
-    testDir: './test/e2e',
-    retries: 5,
-    use: {
-      ignoreHTTPSErrors: env.ignoreHTTPSErrors
-    }
-  }
-)
+  testDir: './test/e2e',
+  retries: 5,
+  use: {
+    ignoreHTTPSErrors: env.ignoreHTTPSErrors,
+  },
+})
