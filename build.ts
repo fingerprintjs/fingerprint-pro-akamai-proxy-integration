26e4ba3d8cde5847726e112dfdfd0e5e5f1af924
Eray Aydın <eray.aydin@fingerprint.com>
2026-04-29 18:20:53 +0300

    ci: test hostname validation

--- build.ts
+++ build.ts
@@ -4,6 +4,8 @@ import fs from 'fs'
 import path from 'path'
 import generateTerraformPropertyRules from './generators/generateTerraformPropertyRules'
 
+console.log('testing ci')
+
 const args = arg({
   '--type': String,
   '--integration-path': String,
